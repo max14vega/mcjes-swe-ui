5eb26d870a2582c41eb47f133a8f0630321e3632
Cheyenne Williams <123600723+cwilliams1123@users.noreply.github.com>
2025-02-17 23:12:29 -0500

    Added logo components to navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppBar, Button, Toolbar, Typography} from "@mui/material";
+import { AppBar, Button, Toolbar, Box } from "@mui/material";
 import { Link } from "react-router-dom";
 import HomeIcon from '@mui/icons-material/Home';  // Icon for Home
 import InfoIcon from '@mui/icons-material/Info';  // Icon for About
@@ -9,18 +9,28 @@ const Navbar = () => {
   return (
     <AppBar position="static" color="primary">
       <Toolbar>
-        <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          Insert Logo
-        </Typography>
-        <Button color="inherit" startIcon={<HomeIcon />} component={Link} to="/">
-          Home
-        </Button>
-        <Button color="inherit" startIcon={<InfoIcon />} component={Link} to="/about">
-          About
-        </Button>
-        <Button color="inherit" startIcon={<ContactSupportIcon />} component={Link} to="/contact">
-          Contact Us
-        </Button>
+        <Box sx={{ flexGrow: 1, display: 'flex', alignItems: 'center', padding: '0 10px' }}> 
+        <Link to="/">
+         <img src="/Images/LogoWhite.png" alt="Logo" style={{ height: '50px', marginRight: '25px' }} />
+        </Link>
+          <Button color="inherit" startIcon={<HomeIcon />} component={Link} to="/">
+            Home
+          </Button>
+          <Button color="inherit" startIcon={<InfoIcon />} component={Link} to="/about">
+            About
+          </Button>
+          <Button color="inherit" startIcon={<ContactSupportIcon />} component={Link} to="/contact">
+            Contact Us
+          </Button>
+        </Box>
+        <Box sx={{ display: 'flex' }}>
+          <Button variant="contained" color="secondary" component={Link} to="/register" sx={{ marginRight: 1, fontWeight: 'bold' }}>
+            Register
+          </Button>
+          <Button variant="contained" color="secondary" component={Link} to="/login" sx={{ fontWeight: 'bold' }}>
+            Log In
+          </Button>
+        </Box>
       </Toolbar>
     </AppBar>
   );
