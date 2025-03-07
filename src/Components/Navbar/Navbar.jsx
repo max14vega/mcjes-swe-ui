d126d2a793322d975786c3351d20cfa40b8198d6
SaadatRafin <68865763+SaadatRafin@users.noreply.github.com>
2025-03-07 11:36:24 -0500

    Add Manuscripts button to Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import ContactSupportIcon from "@mui/icons-material/ContactSupport"; // Icon for
 import HomeIcon from "@mui/icons-material/Home"; // Icon for Home
 import InfoIcon from "@mui/icons-material/Info"; // Icon for About
 import ScienceIcon from "@mui/icons-material/Science"; // Icon for Testing
+import ArticleIcon from '@mui/icons-material/Article'; // Icon for Manuscripts
 import { AppBar, Box, Button, Toolbar } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
@@ -66,6 +67,15 @@ const Navbar = () => {
             to="/testing"
           >
             Testing
+          </Button>
+          <Button // New Manuscripts button
+            variant="contained"
+            color="primary"
+            startIcon={<ArticleIcon />} // Use the Article icon
+            component={Link}
+            to="/manuscripts"
+          >
+            Browse Manuscripts
           </Button>
         </Box>
         <Box sx={{ display: "flex" }}>
