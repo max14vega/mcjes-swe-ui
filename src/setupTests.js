093bde1fe152afe194b36b92e5ac4d51eced23e3
Cheyenne Williams <123600723+cwilliams1123@users.noreply.github.com>
2025-03-10 18:44:07 -0400

    Adding jest adjustments for navbar

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,17 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+
+//for nav bar screen minimizing
+beforeAll(() => {
+  window.matchMedia = jest.fn().mockImplementation((query) => ({
+    matches: true,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated but included for completeness
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  }));
+});
