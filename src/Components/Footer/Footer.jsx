f1cca147712f16b123770f304d99a70c69ddfa1b
Cheyenne Williams <123600723+cwilliams1123@users.noreply.github.com>
2025-03-18 18:31:28 -0400

    Added initial/updated footer design

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,22 +1,47 @@
-import { Box, Typography } from "@mui/material";
-import React from "react";
+import React from 'react';
+import { Box, Typography, Grid, Link, Container } from '@mui/material';
 
 const Footer = () => {
   return (
     <Box
       sx={{
-        bgcolor: "background",
-        color: "primary.main",
-        p: 3,
-        textAlign: "center",
-        position: "relative",
-        bottom: 0,
-        width: "100%",
+        bgcolor: 'background.paper', 
+        color: 'text.primary', 
+        py: 2, 
+        mt: 4, 
+        borderTop: '1px solid #cccccc' 
       }}
     >
-      <Typography variant="body2">
-        &copy; {new Date().getFullYear()} MCJES Journal
-      </Typography>
+      <Container maxWidth="lg">
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={4}>
+            <Typography variant="subtitle1" gutterBottom>Resources</Typography>
+            <Link href="#" color="inherit">About Insects</Link><br />
+            <Link href="#" color="inherit">Insect Identification</Link><br />
+            <Link href="#" color="inherit">Habitats</Link><br />
+            <Link href="#" color="inherit">Photography Tips</Link><br />
+            <Link href="#" color="inherit">Submission Guidelines</Link>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Typography variant="subtitle1" gutterBottom>Community</Typography>
+            <Link href="#" color="inherit">Forums</Link><br />
+            <Link href="#" color="inherit">Recent Activity</Link><br />
+            <Link href="#" color="inherit">Members</Link><br />
+            <Link href="#" color="inherit">Groups</Link><br />
+            <Link href="#" color="inherit">Events</Link>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Typography variant="subtitle1" gutterBottom>Help & Support</Typography>
+            <Link href="#" color="inherit">FAQ</Link><br />
+            <Link href="#" color="inherit">Contact Us</Link><br />
+            <Link href="#" color="inherit">Privacy Policy</Link><br />
+            <Link href="#" color="inherit">Terms of Use</Link>
+          </Grid>
+        </Grid>
+        <Typography variant="body2" sx={{ mt: 2 }}>
+          &copy; {new Date().getFullYear()} Insects Here and Now | All Rights Reserved
+        </Typography>
+      </Container>
     </Box>
   );
 };
